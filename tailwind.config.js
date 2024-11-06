9eeced255939d2cd2d55fc1a9daeb8cb9fbe2f5f
ardszsantos <lokgamer01@gmail.com>
2024-11-06 16:52:34 -0300

    added the main color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "backgroundMain": "#C8E6C9"
+      }
+    },
   },
   plugins: [],
 }
